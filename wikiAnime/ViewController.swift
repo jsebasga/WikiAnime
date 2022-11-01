01afca2a0e0b54362d782a3d2bebb906381a2016
Sebastian Güiza <juanitoguiza@gmail.com>
2022-11-01 07:53:32 -0500

    Initial Commit

---
+++ wikiAnime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wikiAnime
+//
+//  Created by Sebas's Mac on 1/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
