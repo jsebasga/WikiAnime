f18ffd0e8278bbe4356e1db76758acf506c6646b
Sebastian Güiza <juanitoguiza@gmail.com>
2023-02-28 13:01:15 -0500

    Add new API Methods

--- wikiAnime/Model/ApiManager.swift
+++ wikiAnime/Model/ApiManager.swift
@@ -10,18 +10,19 @@ import Foundation
 protocol ApiManagerDelegate {
     
     func didGetSeries(series: [Anime])
+    func didGetSerie(serie: Serie)
     func didFailWithError(error: Error)
 }
 
 struct ApiManager {
     
-    let serieURL = "https://kitsu.io/api/edge/anime?page[limit]=10&page[offset]=0"
+    let animesListURL = "https://kitsu.io/api/edge/anime/"
     
     var delegate: ApiManagerDelegate?
     
     func getAnimesList(){
         
-        if let url = URL(string: serieURL){
+        if let url = URL(string: animesListURL){
             
             let session = URLSession(configuration: .default)
             
@@ -52,6 +53,57 @@ struct ApiManager {
         
         do {
             let animeData = try decoder.decode(AnimesListData.self, from: serieJSON)
+            
+            return animeData
+            
+        } catch {
+            
+            delegate?.didFailWithError(error: error)
+            return nil
+        }
+    }
+    
+    func getAnimeSerie(serieId: String){
+        
+        let serieURL = "\(animesListURL)\(serieId)"
+        print(serieURL)
+        
+        if let url = URL(string: serieURL){
+            
+            let session = URLSession(configuration: .default)
+            
+            let task = session.dataTask(with: url) { data, response, error in
+                
+                if let safeError = error {
+                    
+                    self.delegate?.didFailWithError(error: safeError)
+                    return
+                }
+                
+                //Si data no es nulo, guarde el valor en safeData
+                if let safeData = data {
+                    
+                    //Si no es nulo el parseSerieJSon (este transforma los datos de JSon a Swift), se guarda en la constante serie
+                    if let serie:DetailSerieData = self.parseSerieJson(safeData){
+                        
+                        // Notificacion al delegado que ya esta el objeto serie
+                        self.delegate?.didGetSerie(serie: serie.data)
+                        
+                        print(serie)
+                    }
+                }
+            }
+            
+            task.resume()
+        }
+    }
+    
+    func parseSerieJson(_ serieJSON: Data) -> DetailSerieData? {
+        
+        let decoder = JSONDecoder()
+        
+        do {
+            let animeData = try decoder.decode(DetailSerieData.self, from: serieJSON)
             
             return animeData
             
